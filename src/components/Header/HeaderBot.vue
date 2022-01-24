6be5c07ddd744125a3d934f5232bce6890c2028c
Dandav2923 <daniele.emili23@gmail.com>
2022-01-24 16:31:29 +0100

    navbar dell'header bottom

--- src/components/Header/HeaderBot.vue
+++ src/components/Header/HeaderBot.vue
@@ -1,6 +1,49 @@
 <template>
-  <div class="contaiener">
-
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a 
+              class="nav-link ps-0 ms-0 text-uppercase text-decoration-none" 
+              href="#">home </a>
+          </li>
+          <li class="nav-item">
+            <a 
+              class="nav-link text-uppercase text-decoration-none" 
+              href="#">elements</a>
+          </li>
+          <li class="nav-item">
+            <a 
+              class="nav-link text-uppercase text-decoration-none" 
+              href="#">features</a>
+            </li>
+            <li class="nav-item">
+              <a 
+                href="#"
+                class="nav-link text-uppercase text-decoration-none">pages</a>
+            </li>
+            <li class="nav-item">
+            <a 
+              class="nav-link ps-0 text-uppercase text-decoration-none" 
+              href="#">portfolio </a>
+          </li>
+          <li class="nav-item">
+            <a 
+              class="nav-link ps-0 text-uppercase text-decoration-none" 
+              href="#">blog </a>
+          </li>
+          <li class="nav-item">
+            <a 
+              class="nav-link ps-0 text-uppercase text-decoration-none" 
+              href="#">shop </a>
+          </li>
+        </ul>
+        <span>
+          <i class="fas fa-search"></i>
+        </span>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -10,6 +53,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style style lang="scss" scoped>
+@import "../../assets/scss/partials/_variables.scss";
+.navbar-expand-lg .navbar-nav .nav-link {
+  font-weight:bold ;
+  margin: 0 1.8em 0 1.8em;
+}
 </style>

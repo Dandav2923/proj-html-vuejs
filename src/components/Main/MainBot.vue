dc2a31bcbc182ac0766f00268821bc47f13c32f1
Dandav2923 <daniele.emili23@gmail.com>
2022-01-25 16:22:43 +0100

    main bot senza rifiniture

--- src/components/Main/MainBot.vue
+++ src/components/Main/MainBot.vue
@@ -1,6 +1,292 @@
 <template>
   <div class="container">
-    
+    <div class="row my-5">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-9">
+            <div class="container-post">
+              <div class="container-img">
+                <img class="w-100" src="../../assets/img/blog-54.jpg" alt="">
+              </div>
+              <div class="container-text">
+                <div class="row">
+                  <div class="col-1 my-4">
+                    <div class="row flex-column">
+                      <div class="col text-center ">
+                        <span class="d-inline-block p-4 num-data fs-5">12</span> 
+                      </div>
+                      <div class="col text-center">
+                        <span class="d-inline-block px-4 mese fs-6">jan</span> 
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-11 my-4">
+                    <h2>
+                      titolo
+                    </h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus ratione expedita eligendi repellat voluptates repudiandae tenetur maxime fugiat, sit voluptatem praesentium iusto dicta, aliquid sint facilis tempore unde a!
+                    </p>
+                    <div class="container-links-post d-flex justify-content-between">
+                      <div class="links">
+                        <i class="far fa-user mx-1"> By John Doe </i>
+                        <i class="far fa-folder mx-1"> Lifestyle, Travel </i>
+                        <i class="far fa-comments mx-1"> 12 Comments </i>
+                      </div>
+                      <div class="button-read-more">
+                        <button class="text-uppercase">read more</button>
+                      </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="container-post">
+              <div class="container-img">
+                <img class="w-100" src="../../assets/img/blog-55 (1).jpg" alt="">
+              </div>
+              <div class="container-text">
+                <div class="row">
+                  <div class="col-1 my-4">
+                    <div class="row flex-column">
+                      <div class="col text-center ">
+                        <span class="d-inline-block p-4 num-data fs-5">12</span> 
+                      </div>
+                      <div class="col text-center">
+                        <span class="d-inline-block px-4 mese fs-6">jan</span> 
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-11 my-4">
+                    <h2>
+                      titolo
+                    </h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus ratione expedita eligendi repellat voluptates repudiandae tenetur maxime fugiat, sit voluptatem praesentium iusto dicta, aliquid sint facilis tempore unde a!
+                    </p>
+                    <div class="container-links-post d-flex justify-content-between">
+                      <div class="links">
+                        <i class="far fa-user mx-1"> By John Doe </i>
+                        <i class="far fa-folder mx-1"> Lifestyle, Travel </i>
+                        <i class="far fa-comments mx-1"> 12 Comments </i>
+                      </div>
+                      <div class="button-read-more">
+                        <button class="text-uppercase">read more</button>
+                      </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="container-post">
+              <div class="container-img">
+                <img class="w-100" src="../../assets/img/blog-56 (1).jpg" alt="">
+              </div>
+              <div class="container-text">
+                <div class="row">
+                  <div class="col-1 my-4">
+                    <div class="row flex-column">
+                      <div class="col text-center ">
+                        <span class="d-inline-block p-4 num-data fs-5">12</span> 
+                      </div>
+                      <div class="col text-center">
+                        <span class="d-inline-block px-4 mese fs-6">jan</span> 
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-11 my-4">
+                    <h2>
+                      titolo
+                    </h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus ratione expedita eligendi repellat voluptates repudiandae tenetur maxime fugiat, sit voluptatem praesentium iusto dicta, aliquid sint facilis tempore unde a!
+                    </p>
+                    <div class="container-links-post d-flex justify-content-between">
+                      <div class="links">
+                        <i class="far fa-user mx-1"> By John Doe </i>
+                        <i class="far fa-folder mx-1"> Lifestyle, Travel </i>
+                        <i class="far fa-comments mx-1"> 12 Comments </i>
+                      </div>
+                      <div class="button-read-more">
+                        <button class="text-uppercase">read more</button>
+                      </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="container-post">
+              <div class="container-img">
+                <img class="w-100" src="../../assets/img/blog-58 (1).jpg" alt="">
+              </div>
+              <div class="container-text">
+                <div class="row">
+                  <div class="col-1 my-4">
+                    <div class="row flex-column">
+                      <div class="col text-center ">
+                        <span class="d-inline-block p-4 num-data fs-5">12</span> 
+                      </div>
+                      <div class="col text-center">
+                        <span class="d-inline-block px-4 mese fs-6">jan</span> 
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-11 my-4">
+                    <h2>
+                      titolo
+                    </h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus ratione expedita eligendi repellat voluptates repudiandae tenetur maxime fugiat, sit voluptatem praesentium iusto dicta, aliquid sint facilis tempore unde a!
+                    </p>
+                    <div class="container-links-post d-flex justify-content-between">
+                      <div class="links">
+                        <i class="far fa-user mx-1"> By John Doe </i>
+                        <i class="far fa-folder mx-1"> Lifestyle, Travel </i>
+                        <i class="far fa-comments mx-1"> 12 Comments </i>
+                      </div>
+                      <div class="button-read-more">
+                        <button class="text-uppercase">read more</button>
+                      </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="container-post">
+                <div class="row row-cols-3 p-0">
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-13.jpg" alt="">
+                  </div>
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-16.jpg" alt="">
+                  </div>
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-20.jpg" alt="">
+                  </div>
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-23.jpg" alt="">
+                  </div>
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-29.jpg" alt="">
+                  </div>
+                  <div class="col p-0">
+                    <img class="w-100" src="../../assets/img/blog-40.jpg" alt="">
+                  </div>
+                </div>
+                <div class="container-text">
+                  <div class="row">
+                    <div class="col-1 my-4">
+                      <div class="row flex-column">
+                        <div class="col text-center ">
+                          <span class="d-inline-block p-4 num-data fs-5">12</span> 
+                        </div>
+                        <div class="col text-center">
+                          <span class="d-inline-block px-4 mese fs-6">jan</span> 
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-11 my-4">
+                      <h2>
+                        titolo
+                      </h2>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus ratione expedita eligendi repellat voluptates repudiandae tenetur maxime fugiat, sit voluptatem praesentium iusto dicta, aliquid sint facilis tempore unde a!
+                      </p>
+                      <div class="container-links-post d-flex justify-content-between">
+                        <div class="links">
+                          <i class="far fa-user mx-1"> By John Doe </i>
+                          <i class="far fa-folder mx-1"> Lifestyle, Travel </i>
+                          <i class="far fa-comments mx-1"> 12 Comments </i>
+                        </div>
+                        <div class="button-read-more">
+                          <button class="text-uppercase">read more</button>
+                        </div>
+                      </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <div class="col-3">
+            <h4>Latest from twitter</h4>
+            <div class="row mb-3">
+              <div class="col-1">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class="col-11">
+                <p class="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam autem animi repellendus 
+                </p> 
+                <span>01:04 PM sep 18th</span>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-1">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class="col-11">
+                <p class="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                </p>
+                <span>01:04 PM sep 18th</span>
+              </div>
+            </div>
+            <h4>Photos From instagram</h4>
+            <div class="row row-cols-3 m-0">
+              <div class="col p-0">
+                <img 
+                  class="w-100" 
+                  src="../../assets/img/29739607_2020680068220520_4509928046932787200_n.jpg" 
+                  alt="">
+              </div>
+              <div class="col p-0">
+                <img 
+                class="w-100" 
+                src="../../assets/img/30087804_253872848488989_8792603541668626432_n.jpg" alt="">
+              </div>
+              <div class="col p-0">
+                <img 
+                  class="w-100" 
+                  src="../../assets/img/29415620_196477127626244_3250318472361541632_n.jpg" alt="">
+              </div>
+              <div class="col p-0">
+                <img 
+                  class="w-100" 
+                  src="../../assets/img/30078414_1274410412703843_8290935809419247616_n.jpg" alt="">
+              </div>
+              <div class="col p-0">
+                <img 
+                  class="w-100" 
+                  src="../../assets/img/30077195_2066750973610181_3733150062893596672_n.jpg" alt="">
+              </div>
+              <div class="col p-0">
+                <img 
+                  class="w-100" 
+                  src="../../assets/img/29415304_166583630713703_1032667922171953152_n.jpg" alt="">
+              </div>
+            </div>
+            <h4>
+              tags
+            </h4>
+            <div class="container-buttons">
+              <button>design</button>
+              <button>brands</button>
+              <button>video</button>
+              <button>business</button>
+              <button>travel</button>
+            </div>
+            <h4>
+              find us on facebook
+            </h4>
+            <div class="container-facebook border-start border-5">
+              <span class="d-inline-block p-2">
+                okler themes
+              </span> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +296,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../assets/scss/partials/_variables.scss";
+.num-data {
+  background-color: $bg-color-white7;
+}
+.mese {
+  background-color: $bg-color-semi-black;
+  color: $text-color-grey;
+}
+
+
 
 </style>

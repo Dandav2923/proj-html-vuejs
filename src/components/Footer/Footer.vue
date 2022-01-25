0863e75561fba056bbd901513e163b3a85b534a9
Dandav2923 <daniele.emili23@gmail.com>
2022-01-25 19:47:51 +0100

    footer quasi completo

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,13 +1,94 @@
 <template>
-  <div class="container-fluid">
+<footer>
+  <div class="container-fluid position-relative bg-dark">
     <div class="container">
-      <div class="row justify-content-center">
+      <div class="row justify-content-center py-5">
         <div class="col-12">
           <div class="row row-cols-4">
-            <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi impedit magnam dolores eum dolorum quae accusantium, ratione libero reprehenderit sed rem itaque asperiores et. Explicabo cumque obcaecati temporibus rerum. Itaque.</div>
-            <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quidem iste reprehenderit impedit modi totam incidunt mollitia atque facilis minus perferendis eligendi, explicabo dicta, dignissimos consequatur iusto delectus fugit ab.</div>
-            <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officiis fugiat repellendus nulla sequi cum esse, perferendis quas et provident illo ipsa, veritatis eum pariatur deserunt id, quod laudantium hic?</div>
-            <div class="col">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore eveniet esse laudantium sapiente, harum aperiam tempore suscipit maxime quis exercitationem molestias rerum quas aliquam beatae magni laborum nemo alias commodi?</div>
+            <div class="col about">
+              <h4 class="text-uppercase fs-6">about the blog</h4>
+              <p class="text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam delectus explicabo sunt amet eaque distinctio iste expedita minima quo similique
+              </p>
+              <p class="text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem labore, modi aspernatur voluptas nobis in! Nobis incidunt quidem earum est laboriosam aliquid blanditiis.
+              </p>
+              <h4 class="text-uppercase fs-6">
+                view more 
+                <i class="fas fa-chevron-right"></i>
+              </h4>
+            </div>
+            <div class="col recent-post">
+              <h4 class="text-uppercase fs-6">Recent post</h4>
+              <ul class="list-group list-group-flush ">
+                <li class="list-group-item p-0 my-2 d-flex bg-dark" >
+                  <img class="w-25 my-3 rounded-pill" src="../../assets/img/our-office-4-square.jpg" alt="">
+                  <div class="container-info p-3 text-white">
+                    <h5>titolo articolo</h5>
+                    <p class="text-grey">data articolo</p>
+                  </div>
+                </li>
+                <li class="list-group-item p-0 my-2 d-flex bg-dark">
+                  <img class="w-25 my-3 rounded-pill" src="../../assets/img/our-office-5-square.jpg" alt="">
+                  <div class="container-info p-3 text-white">
+                    <h5>titolo articolo</h5>
+                    <p class="text-grey">data articolo</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="col comments">
+              <h4 class="text-uppercase fs-6">Recent comments</h4>
+              <div class="container-comments">
+                <i class="fas fa-chevron-right color-blue me-2"></i>
+                <span>john doe commented on <a class="text-decoration-none" href="#">lorem lorem lorem</a></span>
+                <p class="text-grey">data articolo</p>
+                <i class="fas fa-chevron-right color-blue me-2"></i>
+                <span>john doe commented on <a class="text-decoration-none" href="#">lorem lorem lorem</a></span>
+                <p class="text-grey">data articolo</p>
+              </div>
+            </div>
+            <div class="col categories">
+              <h4 class="text-uppercase fs-6">categories</h4>
+              <ul class="list-group">
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+                <li class="list-group-item w-50 d-flex">
+                  <button type="button" class="btn text-uppercase">photography
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -16,8 +97,12 @@
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium magni aut optio aliquam possimus, natus consectetur odit distinctio laboriosam. Provident, beatae adipisci? Adipisci est maiores nemo repellendus nulla ex officia!
         </div>
       </div>
+      <div class="container-absolute">
+        <h3>Get in touch</h3>
+      </div>
     </div>
   </div>
+</footer>
 </template>
 
 <script>
@@ -26,6 +111,32 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../../assets/scss/partials/_variables.scss";
+.col, .col-12{
+  color: white;
+}
+.container-absolute {
+  display: inline-block;
+  color: $text-color-white;
+  background-color: $bg-color-blue;
+  position: absolute;
+  top: -30px;
+  left: 283px;
+  // box-shadow: 5px 5px 8px 8px blue,
+  // box-shadow: 5px -10px  blue;
+  h3 {
+    padding: 0.5em 0.7em;
+    font-size: 1.5em;
+  }
+}
+.text-grey {
+  color: $text-color-dark-grey2;
+}
+.text-white {
+  color: $text-color-white;
+}
+.color-blue {
+  color: $text-color-blue;
+}
 </style>
--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container-fluid p-0">
+<div class="container-fluid p-0 my-5">
     <MainTop />
     <MainMid />
     <MainBot />
